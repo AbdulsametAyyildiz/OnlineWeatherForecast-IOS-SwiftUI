e37431934ffec04c0ba1a8cb0ba4c7d34b5bf368
Abdülsamet Ayyıldız <85462491+AbdulsametAyyildiz@users.noreply.github.com>
2021-08-06 21:59:33 +0300

    Initial Commit

---
+++ OnlineWeatherForecast/OnlineWeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineWeatherForecastApp.swift
+//  OnlineWeatherForecast
+//
+//  Created by Abdussamed on 6.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct OnlineWeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
